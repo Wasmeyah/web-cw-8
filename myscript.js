526e526e368b736b064ba4f231457613f1f3d6f0
Wasmeyah <wasmya2013@gmail.com>
2021-01-07 20:19:51 +0300

    all done

--- myscript.js
+++ myscript.js
@@ -5,10 +5,53 @@ let board = [
 ]
 
 
+let s = "O"
 
-function press(cellNO) {
+function press(x,y) {
 
     // Do stuff
-    console.log(cellNO)
+    console.log(x,y)
+    if (s=="O"){
+        s= "X"
+    } 
+    else{
+        s="O"
+    }
+    board[x][y]=s;
+    let cell = document.getElementById(`${x}${y}`)
+    cell.innerText = s ;
     
+    if ( board[0][0]==s && board[0][1]==s && board[0][2]==s) {
+        document.getElementById('here').innerText = (`${s} wins`)
+       
+    }
+    if ( board[0][0]==s && board[1][0]==s && board[2][0]==s) {
+        document.getElementById('here').innerText = (`${s} wins`)
+        
+    }
+    if ( board[0][0]==s && board[1][1]==s && board[2][2]==s) {
+        document.getElementById('here').innerText = (`${s} wins`)
+        
+    }
+    if ( board[1][0]==s && board[1][1]==s && board[1][2]==s) {
+        document.getElementById('here').innerText = (`${s} wins`)
+      
+    }
+    if ( board[2][0]==s && board[2][1]==s && board[2][2]==s) {
+        document.getElementById('here').innerText = (`${s} wins`)
+       
+    }
+    if ( board[0][1]==s && board[1][1]==s && board[2][1]==s) {
+        document.getElementById('here').innerText = (`${s} wins`)
+      
+    }
+    if ( board[0][1]==s && board[1][2]==s && board[2][2]==s) {
+        document.getElementById('here').innerText = (`${s} wins`)
+       
+    }
+    if ( board[2][0]==s && board[1][1]==s && board[0][2]==s) {
+        document.getElementById('here').innerText = (`${s} wins`)
+        
+    }
 }
+
